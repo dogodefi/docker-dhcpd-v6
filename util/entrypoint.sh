a5a9ee12b4ef862c77a75a0e62122cbd46055421
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-06-21 17:01:37 +0800

    Update entrypoint.sh

--- util/entrypoint.sh
+++ util/entrypoint.sh
@@ -48,6 +48,12 @@ if [ -n "$IFACE" ]; then
         echo "Run the container with arguments 'man dhcpd.conf' if you need help with creating the configuration."
         exit 1
     fi
+    dhcpd6_conf="$data_dir/dhcpd6.conf"
+    if [ ! -r "$dhcpd6_conf" ]; then
+        echo "Please ensure '$dhcpd6_conf' exists and is readable."
+        echo "Run the container with arguments 'man dhcpd6.conf' if you need help with creating the configuration."
+        exit 1
+    fi
 
     uid=$(stat -c%u "$data_dir")
     gid=$(stat -c%g "$data_dir")
@@ -60,16 +66,22 @@ if [ -n "$IFACE" ]; then
 
     [ -e "$data_dir/dhcpd.leases" ] || touch "$data_dir/dhcpd.leases"
     chown dhcpd:dhcpd "$data_dir/dhcpd.leases"
+    [ -e "$data_dir/dhcpd6.leases" ] || touch "$data_dir/dhcpd6.leases"
+    chown dhcpd:dhcpd "$data_dir/dhcpd6.leases"
     if [ -e "$data_dir/dhcpd.leases~" ]; then
         chown dhcpd:dhcpd "$data_dir/dhcpd.leases~"
+    if [ -e "$data_dir/dhcpd6.leases~" ]; then
+        chown dhcpd:dhcpd "$data_dir/dhcpd6.leases~"
     fi
 
     container_id=$(grep docker /proc/self/cgroup | sort -n | head -n 1 | cut -d: -f3 | cut -d/ -f3)
     if perl -e '($id,$name)=@ARGV;$short=substr $id,0,length $name;exit 1 if $name ne $short;exit 0' $container_id $HOSTNAME; then
         echo "You must add the 'docker run' option '--net=host' if you want to provide DHCP service to the host network."
     fi
-
-    $run /usr/sbin/dhcpd -$DHCPD_PROTOCOL -f -d --no-pid -cf "$data_dir/dhcpd.conf" -lf "$data_dir/dhcpd.leases" $IFACE
+    #run ipv4
+    $run /usr/sbin/dhcpd -4 -f -d --no-pid -cf "$data_dir/dhcpd.conf" -lf "$data_dir/dhcpd.leases" $IFACE
+    #run ipv6
+    $run /usr/sbin/dhcpd -6 -f -d --no-pid -cf "$data_dir/dhcpd6.conf" -lf "$data_dir/dhcpd6.leases" $IFACE
 else
     # Run another binary
     $run "$@"
